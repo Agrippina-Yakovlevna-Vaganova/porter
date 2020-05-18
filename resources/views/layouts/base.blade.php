90a5d0c0c5dfaed8e026fb5eafc759a6eb91fcd6
L-Size615 <aviexmera@gmail.com>
2020-05-18 04:10:18 +0000

    correction

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
  <meta charset="UTF-8" name="viewport" content="width=device-width,initial-scale=1">
- <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+ <link rel="stylesheet" href="{{ secure_asset('css/welcome.css') }}">
  <title>@yield('title')</title>
 </head>
  <body>
