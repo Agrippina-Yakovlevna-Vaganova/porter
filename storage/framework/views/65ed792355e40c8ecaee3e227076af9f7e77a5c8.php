3e75451e8ce92243f10e1398d93193cffbdf8be7
Agrippina-Yakovlevna-Vaganova <aviexmera@gmail.com>
2020-05-25 12:00:51 +0900

    modi

---
+++ storage/framework/views/65ed792355e40c8ecaee3e227076af9f7e77a5c8.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+ <meta charset="UTF-8" name="viewport" content="width=device-width,initial-scale=1">
+ <link rel="stylesheet" href="<?php echo e(secure_asset('css/welcome.css')); ?>">
+ <title><?php echo $__env->yieldContent('title'); ?></title>
+</head>
+ <body>
+  <?php echo $__env->yieldContent('body'); ?>
+ </body>
+</html><?php /**PATH /home/xs935265/production/laravel/resources/views/layouts/base.blade.php ENDPATH**/ ?>
